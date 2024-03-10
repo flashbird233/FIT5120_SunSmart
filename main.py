4f8565b0652fab96b10c37b7bd5875aaa4523dc0
flashbird233 <moqianchen9811@gmail.com>
2024-03-11 04:19:32 +1100

    Test for requests

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import streamlit as st
+import requests
 
+
+# Test for Streamlit
 st.title('Hello World')
 st.write('This is a simple Streamlit app.')
 
-# Path: requirements.txt
-# streamlit==1.32.0
+# Test for requests
+lat = -37.8136
+lon = 144.9631
+key = "d32542473437f300dfdec104552b7f65"
+main_url = "https://api.openweathermap.org/data/3.0/onecall?"
+req_url = main_url + "lat=" + str(lat) + "&lon=" + str(lon) + "&appid=" + key
+# Get the UV level
+response = requests.get(req_url)
+st.write(response.json())
 
